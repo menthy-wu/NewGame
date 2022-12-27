332a6d00a9681fc75bd2805716f5ad2dd85d6fb8
Yile Li <yrli@Yiles-MacBook-Air.local>
2022-12-26 22:15:06 -0500

    added collision

--- Assets/Codes/Player.cs
+++ Assets/Codes/Player.cs
@@ -21,6 +21,26 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        ProcessCollision(other.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+       ProcessCollision(other.gameObject);
+
+    }
+
+    void ProcessCollision(GameObject collider){
+        if(collider.CompareTag("Enemy")){
+            Die();
+        }
+    }
+
+    void Die(){
+
     }
 
     void Move(){
